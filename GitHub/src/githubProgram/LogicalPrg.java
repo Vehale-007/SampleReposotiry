8afd72bda44a7375abda48183fa6af9d958ccda9
Shubham <shubhamvehale007@gmail.com>
2022-04-10 23:31:59 +0530

    5th commit
    prime, perfect, factorial numbers

--- GitHub/src/githubProgram/LogicalPrg.java
+++ GitHub/src/githubProgram/LogicalPrg.java
@@ -1,10 +1,12 @@
 package githubProgram;
 
+import java.util.ArrayList;
+
 public class LogicalPrg {
 
 	public void pallindrome() {
 		int a[] = {123,234,332,121,345,434};
-		int r = 0;
+		int r=0;
 		System.out.print("Pallindrome no. : ");
 		for(int i=0; i<a.length; i++) {
 			int s = 0;
@@ -36,10 +38,53 @@ public class LogicalPrg {
 				System.out.print(s + " ");
 			}
 		}
+		System.out.println();
+	}
+	public void prime() {
+		System.out.print("Prime No. : ");
+		ArrayList l = new ArrayList();
+		for(int i=2; i<=100; i++) {
+			int c = 0;
+			for(int j =2; j<=i; j++) {
+				if(i%j == 0) {
+					c = c+1;
+				}
+			}
+			if(c == 1) {
+				l.add(i);
+			}
+		}
+		System.out.print(l);
+		System.out.println();
+	}
+	public void perfect() {
+		int n = 6;
+		int c = 0;
+		for(int i=1; i<n; i++) {
+			if(n%i == 0) {
+				c = c+i;
+			}
+		}
+		if(c == n) {
+			System.out.println("It is perfect number");
+		}else {
+			System.out.println("It is not perfect number");
+		}
+	}
+	public void factorial() {
+		int n = 5;
+		int b = 1;
+		for(int i = 1; i<= n; i++) {
+			b = b*i;
+		}
+		System.out.println("Factorial of " + n + "! : " + b);
 	}
 	public static void main(String[] args) {
 		LogicalPrg x = new LogicalPrg();
 		x.pallindrome();
 		x.armStrong();
+		x.prime();
+		x.perfect();
+		x.factorial();
 	}
 }
